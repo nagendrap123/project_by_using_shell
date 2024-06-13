b25afdc24e82088e9f503133e9137d48dec3c48a
nagendrap123 <nagendrap0820@gmail.com>
2024-06-13 22:22:07 +0530

    logs

--- install_logs.sh
+++ install_logs.sh
@@ -3,14 +3,14 @@ id=$(echo $UID)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$($0 | cut -d "." -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-if [ id -ne 0 ] 
+if [ $id -ne 0 ] 
 then 
   echo "not a root user"
 else 
   echo "root user"
 fi 
 VALIDATE () {
-    if [$1 -ne 0 ]
+    if [ $1 -ne 0 ]
     then 
       echo "$2 ...installed..FAILURE"
     else 
