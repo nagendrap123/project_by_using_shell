2fe291bc5fe406c443fb26cbc8054b105775f68d
nagendrap123 <nagendrap0820@gmail.com>
2024-06-13 22:13:00 +0530

    logs

---
+++ install_logs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+id=$(echo $UID)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$($0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+if [ id -ne 0 ] 
+then 
+  echo "not a root user"
+else 
+  echo "root user"
+fi 
+VALIDATE () {
+    if [$1 -ne 0 ]
+    then 
+      echo "$2 ...installed..FAILURE"
+    else 
+      echo "$2 ...installed..SUCCESS"
+    fi  
+}    
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "mysql"
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "nginx"
