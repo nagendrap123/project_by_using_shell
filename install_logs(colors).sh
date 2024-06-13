62fd0b088a9344ea692f2b08d97717d76979da90
nagendrap123 <nagendrap0820@gmail.com>
2024-06-13 23:01:21 +0530

    colors

---
+++ install_logs(colors).sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+id=$(echo $UID)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+echo "script started time: $TIMESTAMP"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+if [ $id -ne 0 ] 
+then 
+  echo "not a root user"
+else 
+  echo "root user"
+fi 
+VALIDATE () {
+    if [ $1 -ne 0 ]
+    then 
+      echo -e "$2 ...installed.. $R FAILURE $N"
+    else 
+      echo -e "$2 ...installed..$G SUCCESS $N"
+    fi  
+}    
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "mysql"
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "nginx"
+
+dnf install dockerr -y &>>$LOG_FILE
+VALIDATE $? "something went wrong"
